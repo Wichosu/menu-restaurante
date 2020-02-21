5a96eb1f1feb2aa6330571196d927f6c9e21736a
Wicho <luiscalderonmiranda@gmail.com>
2020-02-20 19:44:10 -0600

    first commit

---
+++ menu.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+int foodPrice[6] = {18, 10, 15, 12, 8, 12}; //precios de la comida
+int normalMenu = 1;
+//Muestra una lista de opciones a elegir para el usuario
+void optionsList(){
+    cout << "1.- Ver menu" << endl;
+    cout << "2.- Reservar mesa especial" << endl;
+    cout << "3.- Pagar" << endl;
+    cout << "4.- Dejar un comentario" << endl;
+    cout << "5.- Salir del programa" << endl; 
+}
+
+/* displayMenu muestra al usuario su el menu, el array foodName[] contiene el nombre de la comida
+    mientras que el array foodPrice[] el precio, los index de ambos se complementan es decir 
+    foodName[0] = "Tacos" y foodPrice[0] = "18", el index 0 de ambos arrays forman la oracion
+    completa del producto y su precio respectivo*/
+
+void displayMenu(){
+    string foodName[6] = {"Tacos", "Empanadas", "Tortas", "Picaditas", "Agua", "Refresco"}; 
+    int i = 0, maxFood = 6;
+    for(i = 0; i < maxFood; i++){
+        if(normalMenu){
+            cout << foodName[i] << "...................." << "$" << foodPrice[i] << endl; 
+        }
+        else{
+            cout << i + 1 << ".- " << foodName[i] << "............." << "$" << foodPrice[i] << endl;
+        }
+    }
+}
+/* Calcula la cuenta de lo que compor el cliente, se hace display de un menu especial
+    para seleccionar que compro el cliente despues cuanto de eso compro y la cuenta
+    se obtiene multiplicando la cantidad por el precio de la comida (el numero se 
+    sustituye por la cantidad del array)*/
+double total(){
+    int food = 1, amount;
+    double bill;
+    normalMenu = 0;
+    while (food != 0){
+        cout << "¿Que ordeno?" << endl;
+        displayMenu();
+        cout << "Ingrese 0 para salir" << endl;
+        cin >> food;
+        if(food == 0){
+            break;
+        }
+        else if(food > 0 && food < 7){
+            cout << "¿Cantidad?" << endl;
+            cin >> amount;
+            bill += (amount * foodPrice[food -1]);
+        }
+        else{
+            cout << "Escoge una comida del 1 al 6" << endl;
+        }
+    }
+    return bill;
+}
+
+/* TODO: aniadir cuenta y mostrarla a la hora de selecionar la opcion de pagar.
+   TODO: Dar cambio.
+    el procedimiento deja al usuario escoger dos opciones 1 pagar con tarjeta y 
+    2 pagar en efectivo, dependiendo de la opcion se solicita insertar la tarjeta/efectivo
+    y se hace la resta con la cuenta, en este caso solo con el efectivo */
+
+void payMethod(){
+    int method;
+    double bill;
+    char money, creditCard;
+    bill = total();
+    cout << "La cuenta es $" << bill << endl;
+    cout << "Elija su metodo de pago" << endl << "1.-Tarjeta de credito" << endl << "2.-Efectivo" << endl;
+    cin >> method;
+    switch (method){
+    case 1:
+        cout << "Ingrese su tarjeta" << endl;
+        cin >> creditCard;
+        if (creditCard == 'y'){
+            cout << "transaccion completada" << endl;
+        }
+        else{
+            cout << "transaccion" << endl;
+        }
+        break;
+    case 2:
+        cout << "Ingrese su efectivo" << endl;
+        cin >> money;
+        break;
+    default:
+        break;
+    }
+}
+
+void bookSeat(){
+    cout << "Inserte el dia" << endl;
+    //TODO: terminar este procedimientos
+}
+
+int main(){
+    int option = 0;
+    const int exit = 5;
+    cout << "Bienvenido por favor escoja una opcion" << endl;
+    do{
+        optionsList();
+        cin >> option;
+        switch (option){
+        case 1:
+            displayMenu();
+            break;
+        case 2:
+            //TODO: function reservar lugar
+            break;
+        case 3:
+            payMethod();
+            break;
+        case 4:
+            //TODO: function leave a comment
+            break;
+        case 5:
+            cout << "Gracias por su preferencia" << endl;
+            break;
+        default:
+            cout << "Escoja una opcion entre 1 y 5";
+            break;
+        }
+    }
+    while (option != exit);
+    return 0;
+}
